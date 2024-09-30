5e4ea59db5409a7b30a3ce0fa694a2412479794d
kazi-a <kazi.arafat@hotmail.com>
2024-09-29 21:57:41 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context { get; set; }
+        public HomeController(MovieContext ctx) => context = ctx;
+
+        public IActionResult Index()
+        {
+            var movies = context.Movies.Include(m => m.Genre)
+                .OrderBy(m => m.Name).ToList();
+            return View(movies);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
